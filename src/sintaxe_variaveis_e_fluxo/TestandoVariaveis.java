4898c767bc1aac9f5336073b777eaa0dabf8a502
israel santucci <israelsantucci2012@gmail.com>
2021-12-04 15:57:20 -0300

    adicionando classe sobre pontos flutuantes

---
+++ src/sintaxe_variaveis_e_fluxo/TestandoVariaveis.java
@@ -0,0 +1,13 @@
+package sintaxe_variaveis_e_fluxo;
+
+public class TestandoVariaveis {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Testando Variavel int");
+		
+		int idade = 30;
+		
+		System.out.println("A idade é: " + idade);
+	}
+}
